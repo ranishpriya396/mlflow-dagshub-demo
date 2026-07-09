afde10e48e7e170cfa5c81d95b93a389ed0dc4d5
ranishpriya396 <ranishpriya396@gmail.com>
2026-07-09 11:33:18 +0530

    code corrected

--- iris-dt.py
+++ iris-dt.py
@@ -1,7 +1,7 @@
 import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
